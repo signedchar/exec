74682e26c4db6ed6106f5f0f6cf84f7df3f63902
Erbeth Charte <echarte@tutanota.com>
2021-03-21 03:05:53 -0400

    1 Problem 1
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    
    Find the sum of all the multiples of 3 or 5 below 1000.
    
    .

---
+++ euler/multiples_3_and_5/mul_35.lisp
@@ -0,0 +1,17 @@
+(defun divisible (number)
+  "Number is divisible for 3 or 5?"
+  (if (or (equal 0 (mod number 3)) (equal 0 (mod number 5)))
+      t
+      nil))
+
+(defun range-below (start stop)
+  "Build a list between `start' to `stop' - 1."
+  (cond ((> start stop) nil)
+	((equal start stop) nil)
+	(t (cons start (range-below (1+ start) stop)))))
+
+(defun sum53 (start stop)
+  "Summatory of the multiples of 3 and 5 between `start' to `stop'."
+  (apply #'+ (remove-if-not #'divisible (range-below start stop))))
+    
+    
