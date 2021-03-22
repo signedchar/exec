a25d57e735cc85a5abde8690725a0ae7b68f6a2c
Erbeth Charte <echarte@tutanota.com>
2021-03-22 10:44:00 -0400

    (nw) using zerop.

--- euler/multiples_3_and_5/mul_35.lisp
+++ euler/multiples_3_and_5/mul_35.lisp
@@ -1,6 +1,6 @@
 (defun divisible (number)
   "Number is divisible for 3 or 5?"
-  (if (or (equal 0 (mod number 3)) (equal 0 (mod number 5)))
+  (if (or (zerop (mod number 3)) (zerop (mod number 5)))
       t
       nil))
 
@@ -13,5 +13,3 @@
 (defun sum53 (start stop)
   "Summatory of the multiples of 3 and 5 between `start' to `stop'."
   (apply #'+ (remove-if-not #'divisible (range-below start stop))))
-    
-    
